49d9857afd7ff0bf0339decc8ae7746deb6a4e0a
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2024-01-16 17:03:01 -0500

    Creación del Json del producto

---
+++ src/data/product.js
@@ -0,0 +1,30 @@
+const product = {
+    id: "237474-110601",
+    title: "CHAQUETA DE LA NASA MULTICOLOR CON CIERRE PARA HOMBRE",
+    subtitle: "NASA",
+    price: 249990,
+    discountRate: null,
+    sizes: [{ size: "S", amount: 6 }, { size: "M", amount: 3 }, { size: "L", amount: 5 }, { size: "XL", amount: 5 }],
+    amount: 14,
+    sizeguide: "https://www.moviesshop.co/arquivos/guia_de_tallas_movies_2023.png",
+    images: [
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175525-800-800?v=638343813380430000&width=800&height=800&aspect=true',
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175526-800-800?v=638343813692600000&width=800&height=800&aspect=true',
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175527-800-800?v=638343813918430000&width=800&height=800&aspect=true',
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175528-800-800?v=638343814091100000&width=800&height=800&aspect=true',
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175530-800-800?v=638343814525630000&width=800&height=800&aspect=true',
+        'https://moviesshopco.vtexassets.com/arquivos/ids/175614-800-800?v=638349695150300000&width=800&height=800&aspect=true'
+    ],
+    description:'Un diseño de otro planeta llegó a Movies para decirte que siempre podrás tener un estilo más auténtico e innovador. Con esta chaqueta de la NASA tus atuendos para los días fríos serán increíbles aciertos que te pondrán a levitar. Cuenta con detalles que le aportan un abrigo único y una seguridad absoluta, como su cierre frontal y capucha, que te mantendrán protegido de la lluvia. Además, con sus bolsillos, se convertirá en la prenda más práctica del clóset y gracias a sus estampados, ¡tendrás un outfit del más allá!',
+    specifications:{
+        country:'Colombia - FABRICANTE/IMPORTADOR: Maquila Internacional de confección',
+        sic:'811007991-8',
+        details:'Diseño: Chaqueta rompevientos con cierre frontal, capucha, bolsillos funcionales, aplique en punto corazón, estampados en frente, manga derecha y posterior. Silueta: Regular fit',
+        colors:'Blanco, negro y rojo'
+    },
+    composition:'100% Poliéster',
+    care:'- Lavar a mano o en máquina - No lavar en seco - No dejar en remojo - No retorcer ni exprimir - Usar agua fría - Usar jabón suave - No usar blanqueador - No blanquear - No usar secadora - No planchar - Secado en tendedero a la sombra',
+    more:'¡Colecciones irrepetibles! Los diseños más galácticos llegaron para darte los mejores estilos del día a día con prendas que se convertirán en tus favoritas de todo el guardarropa. Continúa explorando en Movies y encuentra más chaquetas icónicas como esta.'
+}
+
+export default product
